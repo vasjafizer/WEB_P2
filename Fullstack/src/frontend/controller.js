e7cae7683e1396da2cc01b3b96a43f8669786b1b
Andrashko <yurii.andrashko@uzhnu.edu.ua>
2022-10-31 09:09:11 +0200

    fullstack frontend seading

---
+++ Fullstack/src/frontend/controller.js
@@ -0,0 +1,7 @@
+const frontendController = {
+    index: async(req, res) => {
+        res.render("index");
+    }
+}
+
+module.exports = frontendController;
--- Fullstack/src/index.js
+++ Fullstack/src/index.js
@@ -2,13 +2,24 @@
 const express = require("express");
 const options = require("./options");
 const userRouter = require("./users");
+const frontendRouter = require("./frontend");
 // створюємо новий екземпляр серверу
 const app = express();
 // використовуємо вбудовані функції для декодування json тіла запитів та запитів в переданих через рядок
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
 // визначаэмо шлях для роутера http://localhost:3000/users
 app.use("/users", userRouter);
+
+
+//роздача файлів фронтенду
+app.set("view engine", "ejs");
+app.use(express.static("./public"));
+app.get("/", frontendRouter);
+
+
 //для решти шляхів повертаємо 404
 app.all("*", (req, res) => {
   res.status(404).send("URL not found");
